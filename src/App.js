eee891e8bce2d5d3ff8215dff13b659e8fc486d8
Pablo CABRERA PEREZ <pablo.cabrera@alten.es>
2020-06-10 11:53:19 +0200

    Added code splitting with react lazy and/in router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 
 
-import React, { useCallback } from 'react';
+import React, { useCallback, Suspense } from 'react';
 import './App.css';
-import Home from './pages/Home';
-import Detail from './pages/Detail';
 
 import { Link, Route, useLocation } from 'wouter';
-import SearchResults from './pages/SearchResults';
 import StaticContext from "./context/StaticContext";
 import { GifsContextProvider } from './context/GifsContext';
 import SearchForm from 'components/SearchForm';
-function App() {
+
+// React lazy and react router for code splitting
+const HomePage = React.lazy(() => import('./pages/Home'));
+const ResultsPage = React.lazy(() => import('./pages/SearchResults'));
+const DetailPage = React.lazy(() => import('./pages/Detail'));
+
+export default function App() {
 
   const [path, pushLocation] = useLocation();
   const handleSubmit = useCallback(({ keyword }) => {
-    if(!keyword.trim()) keyword = 'puppys';
+    if (!keyword.trim()) keyword = 'puppys';
     pushLocation(`/search/${keyword}`)
   }, [pushLocation])
 
@@ -27,35 +30,37 @@ function App() {
     }>
       <div className="App">
         <section className="App-content">
+          
           <Link to={'/'}>
             <h1 className='rainbow-text'> Giffy Giff App</h1>
           </Link>
-          
           <Link to={'/'}>
             <img id='logo' src='/gif-logo.png' alt='logo'></img>
           </Link>
-          <GifsContextProvider>
-            <div className='searchBar'>
-              <SearchForm onSubmit={handleSubmit} />
-            </div>
-            <Route
-              path='/'
-              component={Home} />
-            <Route
-              path='/search/:keyword'
-              component={SearchResults} />
-            <Route
-              path='/gif/:id'
-              component={Detail} />
-            <Route
-              path='/404'
-              component={() =>
-                <h1> 404 Error, no gif :( </h1>} />
-          </GifsContextProvider>
+
+          <Suspense fallback={null}>
+            <GifsContextProvider>
+              <div className='searchBar'>
+                <SearchForm onSubmit={handleSubmit} />
+              </div>
+              <Route
+                path='/'
+                component={HomePage} />
+              <Route
+                path='/search/:keyword'
+                component={ResultsPage} />
+              <Route
+                path='/gif/:id'
+                component={DetailPage} />
+              <Route
+                path='/404'
+                component={() =>
+                  <h1> 404 Error, no gif :( </h1>} />
+            </GifsContextProvider>
+          </Suspense>
         </section>
       </div>
     </StaticContext.Provider>
   );
 }
 
-export default App;
